09d172f9a4fb5e5a5280123b6e97a861f9c15a77
shahnewaz171 <shahnewaz601@gmail.com>
2022-06-17 15:02:47 +0600

    fxied cors issues

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
 const app = express();
 
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
   schema,
-  graphiql: true
+  graphiql: true,
 }))
 
 
